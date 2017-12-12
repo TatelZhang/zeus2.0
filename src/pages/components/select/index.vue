a7993518bae39b34f831b704197deb04d473d71b
flyerjay <876173839@qq.com>
2017-12-12 13:44:22 +0800

    封装select

--- src/pages/components/select/index.vue
+++ src/pages/components/select/index.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="zeus-select">
-        <input type="hidden" :value="currentValue">
+        <input
+            ref="input"
+            type="hidden"
+            :value="currentValue">
         <Select 
-            v-model="currentValue"
+            v-model="bindValue"
             :size="size"
             :disabled="disabled"
             :placeholder="placeholder"
@@ -12,23 +15,14 @@
         </Select>
     </div>
 </template>
-<style lang="less">
-    .zeus-select-align-left{ text-align: left }
-    .zeus-select-align-right{ text-align: center }
-    .zeus-select-align-center{ text-align: center }
-</style>
 <script>
+    import Emitter from '../../mixin/emitter';
     const prefixCls = 'zeus-select';
     export default {
-        data () {
-            return {
-                list: [],
-                currentValue: this.value,
-            }
-        },
+        name: 'ZSelect',
+        mixins: [ Emitter ],
         props: {
             value: [String, Number],
-            remote: String,
             size: String,
             disabled: Boolean,
             placeholder: String,
@@ -39,14 +33,20 @@
             data: [Array, String], //data接收数组类型和加载数据的地址
             keyValue: String, //指定key-value,如果不指定默认使用key,value关键字,形如'key-value'
         },
+        data () {
+            return {
+                list: [],
+                currentValue: this.value,
+                prefixCls: prefixCls,
+                bindValue: this.value
+            };
+        },
         computed: {
             alignCls () {
                 return `${prefixCls}-align-${this.align}`;
             }
         },
         created () {
-            console.log("value:" + this.value);
-            console.log("value:" + this.model);
             this.load();
         },
         methods: {
@@ -77,13 +77,21 @@
                     })
                 }
                 this.list = data;
-            }
+            },
+            setCurrentValue (value) {
+                if (value === this.currentValue) return;
+                this.$emit('input', value);
+                this.currentValue = value;
+            },
         },
         watch: {
-            currentValue (v) {
-                this.currentValue = v;
-                console.log(v);
+            value (val) {
+                this.setCurrentValue(val);
+            },
+            bindValue (val) {
+                this.setCurrentValue(val);
             }
-        }
-    }
+        },
+    };
 </script>
+    

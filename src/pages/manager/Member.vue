0039c9a40de3e2a9076a063b2c5082cfc244163a
Tatel <782487339@qq.com>
2018-01-04 21:47:09 +0800

    member页面功能完成

--- src/pages/manager/Member.vue
+++ src/pages/manager/Member.vue
@@ -22,7 +22,7 @@
         </div>
         <div class="info-item">
           <span class="margin-right-10">密码：</span>
-          <Input style="display:inline-block; width:80%;" size="large" v-model="newMember.password"></Input>
+          <Input style="display:inline-block; width:80%;" size="large" v-model="newMember.password" type="password"></Input>
         </div>
         <div class="info-item">
           <span class="margin-right-10">公司：</span>
@@ -44,6 +44,29 @@
       <CheckboxGroup style="font-size: 14px;" v-model="currentAuth">
       <Checkbox v-for="item in authNameList" :key="item.key" :label="item.key" style="font-size: 14px;">{{item.value}}</Checkbox>
       </CheckboxGroup>
+    </Modal>
+    <Modal v-model="modalStatus.authQuick" @on-ok="uploadAuths">
+      <h2 slot="header" style="color:#19be6b;text-align:center">
+        <Icon type="settings"></Icon>
+        <span>快速设置</span>
+      </h2>
+      <div>
+        <div style="font-size: 14px;margin: 0 0 10px;font-weight: bold;">成员类型：</div>
+        <Select v-model="currentType" size="large">
+          <Option v-for="item in quickTypeSelctItems" :key="item.key" :value="item.key">{{item.value}}</Option>
+        </Select>
+        <div>
+          <div style="font-size: 14px;margin: 10px 0;font-weight: bold;">描述：</div>
+          {{currentDesciption}}
+        </div>
+      </div>
+    </Modal>
+    <Modal v-model="modalStatus.passwdReset" @on-cancel="resetParams.password = ''" @on-ok="restPasswd">
+      <h2 slot="header" style="color:#f90;text-align:center">
+        <Icon type="wrench"></Icon>
+        <span>重置密码</span>
+      </h2>
+      <Input type="password" v-model="resetParams.password" placeholder="新的密码" size="large"></Input>
     </Modal>
   </div>
 </template>
@@ -150,10 +173,22 @@
                 }}, 
                 '修改权限'),
                 h('Button', 
-                {props: {type: 'success'}}, 
+                {props: {type: 'success'},
+                on: {
+                  click: ()=>{
+                    this.quickAuth(row)
+                  }
+                }}, 
                 '快速设置'),
                 h('Button', 
-                {props: {type: 'warning'}}, 
+                {props: {type: 'warning'},
+                on: {
+                  click: ()=>{
+                    this.resetParams.resetUser = row.userId
+                    this.modalStatus.passwdReset = true
+                  }
+                }
+                }, 
                 '重置密码'),
                 h('Button', 
                 {props: {type: 'error'},
@@ -188,7 +223,9 @@
           authQuick: false,
           passwdReset: false
         },
+        // 当前成员权限
         currentAuth: [],
+        // 用于上传的数据
         paramsWillBeUpload: {
           operator: 0,
           queryAuth: 0,
@@ -198,6 +235,46 @@
           inventoryAuth: 0,
           demandAuth: 0,
           adminAuth: 0
+        },
+        // 快速设置类型
+        quickTypeSelctItems: [
+          {key: 0, value: '自定义角色', desc: '自定义角色请使用权限分配功能'},
+          {key: 1, value: '销售', desc: '销售拥有基础功能权限+定制化需求权限'},
+          {key: 2, value: '采购', desc: '采购拥有除管理员以外的全部权限'},
+          {key: 3, value: '管理员', desc: '超级管理员拥有系统全部权限'},
+        ],
+        allAuths:['queryAuth', 'orderAuth', 'supplierAuth', 'valueAuth', 'inventoryAuth', 'demandAuth', 'adminAuth'], 
+        currentType: 0,
+        authStr: {
+          '0000000': 0,
+          '0000010': 1,
+          '1111100': 2,
+          '1111111': 3
+        },
+        resetParams: {
+          password: '',
+          resetUser: ''
+        }
+      }
+    },
+    watch: {
+      // 用于监控当前成员类型的改变, 并给予相应的权限
+      currentType(val, old){
+        for(let str in this.authStr){
+          if(this.authStr[str] === val){
+            for(let index in this.allAuths){
+              this.paramsWillBeUpload[this.allAuths[index]] = Number(str[index])
+            }
+          }
+        }
+      }
+    },
+    computed: {
+      currentDesciption () {
+        for(let item of this.quickTypeSelctItems){
+          if(item.key === this.currentType){
+            return item.desc
+          }
         }
       }
     },
@@ -210,8 +287,9 @@
         this.newMember.registerId = ''
         this.newMember.password = ''
         this.newMember.comId = ''
-        console.log(this.newMember)
+        // console.log(this.newMember)
       },
+      // 新增member
       newMemberSubmit () {
         axios.post("/zues/api/user/register", this.newMember).then(res=>{
           if(res.status === 200){
@@ -219,6 +297,7 @@
             if(data.code === 200){
               this.$Message.info(data.msg)
               this.clearNewMember()
+              this.$refs.member.search()
             }else{
               this.$Message.info(data.msg||data.message)
             }
@@ -247,7 +326,7 @@
         })
       },
       listCurrentAuth (row){
-        this.currentAuth = [] // 清除当前权限
+        this.currentAuth = [] // 清除当前权限数据
         this.paramsWillBeUpload.operator = row.userId
         for(var auth in row){
           if(row[auth] === 1){
@@ -260,6 +339,7 @@
           this.paramsWillBeUpload[item] = 0
         }
       },
+      // 更新成员权限
       uploadAuths () {
         for(let auth of this.currentAuth){
           this.paramsWillBeUpload[auth] = 1
@@ -267,15 +347,45 @@
         axios.post('/zues/api/userrole/update', this.paramsWillBeUpload)
         .then(res=>{
           if(res.status === 200){
-                let {data} = res
-                if(data.code === 200){
-                  this.$Message.success(data.msg)
-                  this.$refs.member.search()
-                  this.cleanUploadParams()
-                }else{
-                  this.$Message.error(data.msg)
-                }
-              }
+            let {data} = res
+            if(data.code === 200){
+              this.$Message.success(data.msg)
+              this.$refs.member.search()
+            }else{
+              this.$Message.error(data.msg)
+            }
+          }
+          this.cleanUploadParams()  // 清除需要上传的参数数据
+        })
+      },
+      // 快速设置初始化参数, 首先获取当前成员, 弹出快速设置对话框, 设置当前类型
+      quickAuth (row) {
+        this.paramsWillBeUpload.operator = row.userId
+        this.modalStatus.authQuick = true
+        // let allAuths = ['queryAuth', 'orderAuth', 'supplierAuth', 'valueAuth', 'inventoryAuth', 'demandAuth', 'adminAuth']
+        let code = ''
+        for(let auth of this.allAuths){
+          code += row[auth]
+        }
+        if(this.authStr[code]){
+          this.currentType = this.authStr[code]
+        }else{
+          this.currentType = 0
+        }
+      },
+      restPasswd () {
+        axios.post('/zues/api/user/update', this.resetParams)
+        .then(res=>{
+          console.log(res)
+          if(res.status === 200){
+            let {data} = res
+            if(data.code === 200){
+              this.$Message.info(data.msg)
+              this.resetParams.password = ''
+            }else{
+              this.$Message.info(data.msg||data.message)
+            }
+          }
         })
       }
     }
@@ -299,5 +409,8 @@
     display: inline-block;
     width: 13%;
     font-size: 13px;
+  }
+  .member-container .member-modal .desc{
+    font-size: 14px;margin: 0 0 10px;font-weight: bold;
   }
 </style>

4d2924082b21ad4b03294a9e6362386eb03b089b
Tatel <782487339@qq.com>
2018-01-09 23:43:01 +0800

    supplier 运费录入

--- src/pages/data/Supplier.vue
+++ src/pages/data/Supplier.vue
@@ -16,11 +16,11 @@
       <TabPane label="运费信息">
         <Row>
           <div class="daily-input">
-            <Button type="info" size="large">每日运费录入</Button>
+            <Button type="info" size="large" @click="modalStatus.freightAdd = true">每日运费录入</Button>
           </div>
           <br>
         </Row>
-        <ZTable :column="daily.column" :config="daily.config"></ZTable>
+        <ZTable :column="daily.column" :config="daily.config" ref="daily"></ZTable>
       </TabPane>
     </Tabs>
     <Modal v-model="modalStatus.benifitChange" @on-ok="changeBenifit">
@@ -46,6 +46,23 @@
           <span>政策下浮（元/吨）</span>
         </div>
         <input type="number" class="ivu-input ivu-input-large" v-model="currSupplier.benifit">
+      </div>
+    </Modal>
+    <Modal v-model="modalStatus.freightAdd" @on-ok="addFreight">
+      <h2 slot="header" style="color:#f90;text-align:center">
+        <span>每日运费录入</span>
+      </h2> 
+      <div class="ivu-input-wrapper ivu-input-wrapper-large ivu-input-type ivu-input-group ivu-input-group-large ivu-input-group-with-prepend" style="margin-bottom: 14px">
+        <div class="ivu-input-group-prepend">
+          <span>供应商所在地</span>
+        </div>
+        <input type="value" class="ivu-input ivu-input-large" v-model="freightAdd.address">
+      </div>
+      <div class="ivu-input-wrapper ivu-input-wrapper-large ivu-input-type ivu-input-group ivu-input-group-large ivu-input-group-with-prepend">
+        <div class="ivu-input-group-prepend">
+          <span>运费</span>
+        </div>
+        <input type="number" class="ivu-input ivu-input-large" v-model="freightAdd.freight">
       </div>
     </Modal>
   </div>
@@ -115,10 +132,10 @@
                      type: 'warning'
                    },
                    on: {
-                     click:()=>{
-                       let {address, benifit, benifitAdjust, freight, inventoryTime, isValide, supplierId, supplierName, valueTime} = row
-                       let r = {address, benifit, benifitAdjust, freight, inventoryTime, isValide, supplierId, supplierName, valueTime}
-                       this.showBenifit(r)
+                     click: () => {
+                      //  let {address, benifit, benifitAdjust, freight, inventoryTime, isValide, supplierId, supplierName, valueTime} = row
+                      //  let r = {address, benifit, benifitAdjust, freight, inventoryTime, isValide, supplierId, supplierName, valueTime}
+                       this.showBenifit(row)
                      }
                    }
                  }, '修改优惠政策')
@@ -173,13 +190,18 @@
           }
         },
         modalStatus: {
-          benifitChange: false
+          benifitChange: false,
+          freightAdd: false
         },
         currSupplier: {
           supplierName: '',
           address: '',
-          benifit: null,   // 优惠政策
+          benifit: '',   // 优惠政策
           supplierId: ''
+        },
+        freightAdd: {
+          address: '',
+          freight: "0"
         }
       }
     },
@@ -201,19 +223,46 @@
         })
       },
       showBenifit (row) {
-        console.log(row)
         this.currSupplier.supplierName = row.supplierName
         this.currSupplier.address = row.address
         this.currSupplier.benifit = row.benifit
         this.currSupplier.supplierId = row.supplierId
-        this.currSupplier.row = row
-        this.currSupplier.comId = ''
+        // this.currSupplier.row = row
+        // this.currSupplier.comId = ''
         this.modalStatus.benifitChange = true
       },
       changeBenifit () {
-        axios.post('/zues/api/freight/update', this.currSupplier).then(res=>{
-          console.log(res)
+        axios.post('/zues/api/supplier/update', this.currSupplier).then(res=>{
+          let {status, data} = res
+          if(status === 200 && data.code === 200){
+            this.$Message.success(data.msg)
+            this.searchSupplier()
+          }else{
+            this.$Message.error(data.msg||'信息异常')
+          }
         })
+      },
+      addFreight () {
+        if(this.freightAdd.address){
+          axios.post('/zues/api/freight/add', this.freightAdd).then(res=>{
+            let {status, data} = res
+            if(status){
+              if(data.code === 200){
+                this.$Message.success(data.msg)
+                this.$refs.daily.search()
+                this.freightAdd.address = '',
+                this.freightAdd.freight = "0"
+              }else{
+                this.$Message.error(data.msg)
+              }
+            }
+          })
+        }else{
+          setTimeout(()=>{
+            this.modalStatus.freightAdd = true
+          },0)
+          this.$Message.error("请输入供应商所在地")
+        }
       }
     }
   }
